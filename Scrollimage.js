b741f6d56731ea52a3448593071249fa24dfdb64
BungNawi <40924186+satuaja@users.noreply.github.com>
2022-05-06 23:44:36 +0700

    Delete Scrollimage.js

--- Scrollimage.js
+++
@@ -1,4 +0,0 @@
-function scrolll(){var left=document.querySelector(&quot;.scroll-images&quot;);left.scrollBy(250,0)}
-function scrolll2(){var left=document.querySelector(&quot;.scroll-images2&quot;);left.scrollBy(250,0)}
-function scrollr(){var right=document.querySelector(&quot;.scroll-images&quot;);right.scrollBy(-250,0)}
-function scrollr2(){var right=document.querySelector(&quot;.scroll-images2&quot;);right.scrollBy(-250,0)}
